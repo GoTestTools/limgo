2aacda5f83ff7bc2a16fa69ecccf2047bad7f0f6
Michael Engel <engel.michael@hotmail.de>
2022-10-31 12:40:19 +0100

    moved coverage error types to dedicated file

--- pkg/model/evaluation/error.go
+++ pkg/model/evaluation/error.go
@@ -1,14 +1,5 @@
 package evaluation
 
-type CoverageErrorType string
-
-const (
-	CoverageErrorStmt     CoverageErrorType = "statements"
-	CoverageErrorLines    CoverageErrorType = "lines"
-	CoverageErrorBranches CoverageErrorType = "branches"
-	CoverageErrorFunc     CoverageErrorType = "functions"
-)
-
 type CoverageError struct {
 	Type         CoverageErrorType
 	AffectedFile string
